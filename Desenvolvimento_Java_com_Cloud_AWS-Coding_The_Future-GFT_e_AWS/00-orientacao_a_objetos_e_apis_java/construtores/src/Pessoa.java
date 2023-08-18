ab6c16e875be281d488b8890f16f05c958e0cce5
maiza <021maizadv@gmail.com>
2023-08-18 00:43:23 -0300

    :recycle: refactor: construtor inicialização

--- Desenvolvimento_Java_com_Cloud_AWS-Coding_The_Future-GFT_e_AWS/00-orientacao_a_objetos_e_apis_java/construtores/src/Pessoa.java
+++ Desenvolvimento_Java_com_Cloud_AWS-Coding_The_Future-GFT_e_AWS/00-orientacao_a_objetos_e_apis_java/construtores/src/Pessoa.java
@@ -4,6 +4,17 @@ public class Pessoa {
 	private String cpf;
 	private String endereco;
 	
+	// método construtor
+	// o nome deverá ser igual ao nome da classe
+	public Pessoa (String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+		
+	//...
+	//getters
+	//setters
+
 	public String getNome() {
 		return nome;
 	}
